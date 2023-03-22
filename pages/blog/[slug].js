e87443edbc6e50d327262650f2f48e21c08fa679
Kelvin <kelvinyelyen@gmail.com>
2023-03-22 15:02:43 +0000

    Visual updates

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -8,13 +8,16 @@ export default function Post({ post }) {
         <p className="text-[13px] text-stone-500 my-5">
           ({new Date(post.published_at).toDateString()})
         </p>
-
-        <img src={post.feature_image} alt="" />
-        <h1 className="lg:text-[40px] text-[30px] leading-tight my-5">{post.title}</h1>
-        <div
-          className="leading-7 text-[16px]"
-          dangerouslySetInnerHTML={{ __html: post.html }}
-        />
+        <div className="text-secondary-white">
+          <img src={post.feature_image} alt="" />
+          <h1 className="lg:text-[40px] text-[30px] leading-tight my-5">
+            {post.title}
+          </h1>
+          <div
+            className="leading-7 text-[16px]"
+            dangerouslySetInnerHTML={{ __html: post.html }}
+          />
+        </div>
       </div>
     </section>
   )
