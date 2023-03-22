0e868f112505891ae0e96353a9a5d3c9cc6170a3
Kelvin <kelvinyelyen@gmail.com>
2023-03-22 11:32:29 +0000

    Visual updates

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -8,17 +8,16 @@ const Header = () => {
         className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
       >
         {/* Show this div on large screens only */}
-        <div className="flex-col hidden lg:block dark:text-secondary-white">
+        <div className="flex-col dark:text-secondary-white">
           <Link href="/">
-            <h1 className="font-monument tracking-tighter text-[40px] lg:text-[50px] -mb-2 -mt-4">
+            <h1 className="font-monument tracking-tighter text-[30px] lg:text-[50px] lg:-mb-2 mb-0 lg:-mt-4">
               kelvinyelyen
             </h1>
-            <p className="text-[15px] dark:font-light uppercase">
+            <p className="text-[15px] lg:block hidden dark:font-light uppercase">
               Software Engineer & Designer
             </p>
           </Link>
         </div>
-        <div className="flex"></div>
 
         <ul className="flex gap-5 sm:text-[13px] text-[10px] dark:text-secondary-white">
           <li>
@@ -31,14 +30,14 @@ const Header = () => {
       </div>
 
       {/* Show this div on mobile screens only */}
-      <div className="flex flex-col lg:hidden mt-[200px] dark:text-secondary-white">
+      {/* <div className="flex flex-col lg:hidden mt-[200px] dark:text-secondary-white">
         <h1 className="font-monument text-[40px] tracking-tighter -mb-1">
           Kelvin Yelyen
         </h1>
         <p className="text-[10px] uppercase">
           Software Engineer & Designer
         </p>
-      </div>
+      </div> */}
     </nav>
   )
 }
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -5,12 +5,12 @@ export default function Post({ post }) {
   return (
     <section className={`${styles.paddings} relative z-10`}>
       <div className={`${styles.blogWidth} mx-auto`}>
-        <p className="text-[13px] text-stone-500">
+        <p className="text-[13px] text-stone-500 my-5">
           ({new Date(post.published_at).toDateString()})
         </p>
 
         <img src={post.feature_image} alt="" />
-        <h1 className="text-[40px]">{post.title}</h1>
+        <h1 className="lg:text-[40px] text-[30px] leading-tight my-5">{post.title}</h1>
         <div
           className="leading-7 text-[16px]"
           dangerouslySetInnerHTML={{ __html: post.html }}
