aa8853d1f3f23b8e5fb2213be2738e063176e4d3
Kelvin <kelvinyelyen@gmail.com>
2023-03-22 14:45:58 +0000

    Visual updates

--- components/Home/Selected.js
+++ components/Home/Selected.js
@@ -6,20 +6,20 @@ const Project = ({ number, title, imageUrl, altText, year, type, url }) => {
   return (
     <>
       <Link href={url}>
-        <div className="grid grid-cols-3 sm:grid-cols-3 lg:my-10 my-0 text-end dark:text-secondary-white">
+        <div className="grid grid-cols-2 sm:grid-cols-3 my-10 text-end dark:text-secondary-white">
           <div className="flex">
             <span className="text-stone-500 text-xs sm:text-base mr-1 sm:mr-5">
               <sup>{number}</sup>
             </span>
-            <h2 className="font-monument uppercase tracking-tighter text-[25px] sm:text-4xl">
+            <h2 className="font-monument uppercase tracking-tighter text-[25px] sm:text-4xl text-start">
               {title}
             </h2>
           </div>
-          <div className="text-[10px] lg:ml-0 ml-20 w-20 text-end">
+          <div className="text-[10px]">
             <p>{type}</p>
             <p>/{year}</p>
           </div>
-          <div className="text-[10px]">&#129126;</div>
+          <div className="text-[10px] hidden lg:block">&#129126;</div>
         </div>
       </Link>
 
