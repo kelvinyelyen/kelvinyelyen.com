0c7f8b12785087d44e485ebfab80b3e9c264f77b
Kelvin <kelvinyelyen@gmail.com>
2023-03-22 22:16:11 +0000

    Preloader fix

---
+++ components/Preloader.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react"
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger"
+
+const Preloader = () => {
+    useEffect(() => {
+      const tl = gsap.timeline({
+        onComplete: () => {
+          const preloader = document.querySelector(".preloader")
+          preloader.classList.add("preloader-done")
+        },
+      })
+
+      tl.to(".preloader__text", {
+        opacity: 1,
+        duration: 0.5,
+        ease: "power2.out",
+      })
+        .to(".preloader__text", {
+          opacity: 0,
+          duration: 0.5,
+          ease: "power2.in",
+          delay: 1,
+        })
+        .to(".preloader", {
+          opacity: 0,
+          duration: 0.5,
+          ease: "power2.in",
+          delay: -0.5,
+        })
+    }, [])
+
+    return (
+      <div className="preloader">
+        <div className="preloader__inner">
+          <h1 className="preloader__text text-[20px]">
+            <span className="font-medium">Kelvin Yelyen</span> &nbsp;
+            Portfolio
+          </h1>
+        </div>
+      </div>
+    )
+}
+
+export default Preloader
--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,12 @@ import { ThemeProvider } from "next-themes"
 import Scroller from "@/utils/smoothScroll"
 import Layout from "@/components/Layout"
 import "@/styles/globals.css"
+import Preloader from "@/components/Preloader"
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
+      <Preloader />
       <Scroller>
         <ThemeProvider attribute="class">
           <Layout>
