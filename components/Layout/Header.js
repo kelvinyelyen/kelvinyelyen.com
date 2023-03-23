4a56e8e7ce36ed50fe4fbf1702c89fa2766acd39
Kelvin <kelvinyelyen@gmail.com>
2023-03-23 21:24:16 +0000

    Visual updates

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -7,15 +7,15 @@ const Header = () => {
       <div
         className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
       >
-        <div className="flex-col dark:text-secondary-white">
+        <div className="dark:text-secondary-white">
           <Link href="/">
             <h1 className="font-monument tracking-tighter lg:block hidden lg:text-5xl lg:mb-1">
               kelvinyelyen
             </h1>
-            <h1 className="block lg:hidden text-[30px]">&#10042;</h1>
             <p className="text-sm lg:block hidden dark:font-light uppercase">
               Software Engineer &amp; Designer
             </p>
+            <h1 className="block lg:hidden text-xs">&#10042;</h1>
           </Link>
         </div>
         <ul className="flex gap-5 text-xs dark:text-secondary-white uppercase">
