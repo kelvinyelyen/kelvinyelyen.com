624e41de35be1efd67e5ff5ac16f31febb05a442
Kelvin <kelvinyelyen@gmail.com>
2023-03-26 00:22:02 +0000

    Minor updates

--- components/Home/Work.js
+++ components/Home/Work.js
@@ -9,7 +9,7 @@ const Work = () => {
             <p className="text-[10px] text-stone-500">(Work with me)</p>
             <p className="sm:text-[15px] text-[12px] dark:font-light dark:text-secondary-white">
               I&apos;m currently focusing on software engineering and Product Design,
-              as well as exploring new areas of technology ie. AI and pursuing my
+              as well as exploring new areas of technology eg. AI and pursuing my
               creative interest in design. I&apos;m always seeking new
               opportunities.
             </p>
