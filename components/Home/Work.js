51a012abfe339280bc8dcabdad5a9a1b20fa1404
Kelvin <kelvinyelyen@gmail.com>
2023-03-26 00:20:29 +0000

    Minor updates

--- components/Home/Work.js
+++ components/Home/Work.js
@@ -8,8 +8,8 @@ const Work = () => {
           <article className="uppercase lg:mx-[200px] prose sm:prose-lg">
             <p className="text-[10px] text-stone-500">(Work with me)</p>
             <p className="sm:text-[15px] text-[12px] dark:font-light dark:text-secondary-white">
-              I'm currently focusing on software engineering and Product Design
-              and exploring new areas of technology ie. AI and pursuing my
+              I&apos;m currently focusing on software engineering and Product Design,
+              as well as exploring new areas of technology ie. AI and pursuing my
               creative interest in design. I&apos;m always seeking new
               opportunities.
             </p>
