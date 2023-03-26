f1e8a2a1f6063e6b1db4b5dd852a52aaec7fe862
Kelvin <kelvinyelyen@gmail.com>
2023-03-26 18:17:05 +0000

    Layout adjustments

--- components/Projects/AllProjects.js
+++ components/Projects/AllProjects.js
@@ -8,27 +8,28 @@ const AllProjects = () => {
       <div
         className={`${styles.projectWidth} mx-auto dark:text-secondary-white`}
       >
-        <div className="lg:mr-[50%] mr-0">
-          <p className="uppercase text-[10px] text-stone-500">
-            (Full-Stack, Product Design ...)
-          </p>
-          <p className="text-neutral-700 dark:text-secondary-white text-[13px] md:text-base ">
-            I possess a rare blend of technical abilities and design expertise,
-            which enables me to develop software that not only functions
-            properly but also impresses users with its pleasing appearance,
-            intuitive usage, and user-friendliness. I&apos;ve collaborated on
-            various personal and team projects throughout my career, and here
-            are some that can be mentioned publicly.
-          </p>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          <div className="lg:mr-[20%] mr-0">
+            <p className="uppercase text-[10px] text-stone-500">
+              (Software Eng, Web Apps, Product Design ... )
+            </p>
+            <p className="text-neutral-700 dark:text-secondary-white text-[13px] md:text-base ">
+              I&apos;ve collaborated on various personal and team projects
+              throughout my career, and here are some that can be mentioned
+              publicly.
+            </p>
+          </div>
+
+          <div className="col-span-2">
+            <hr className="h-px lg:mb-8 my-8 lg:my-0 bg-gray-300 dark:bg-neutral-500 border-0" />
+            {projects.map((project) => (
+              <Project key={project.number} {...project} />
+            ))}
+          </div>
         </div>
-        <hr className="h-px my-8 bg-gray-300 dark:bg-neutral-500 border-0" />
-        {projects.map((project) => (
-          <Project key={project.number} {...project} />
-        ))}
       </div>
     </section>
   )
 }
 
 export default AllProjects
-    
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -9,37 +9,41 @@ export default function BlogPost({ posts }) {
       <div
         className={`${styles.projectWidth} mx-auto dark:text-secondary-white`}
       >
-        <div className="lg:mr-[50%] mr-0">
-          <p className="uppercase text-[10px] text-stone-500">
-            (Musings, Shower-thoughts, Essays ...)
-          </p>
-          <p className="text-neutral-700 dark:text-secondary-white text-[13px] md:text-base ">
-            At certain times, articulating a concise portrayal of my identity or
-            occupation can prove to be challenging, as it encompasses a
-            complexity that cannot be adequately conveyed through a mere handful
-            of words.
-          </p>
-        </div>
-        <hr className="h-px my-8 bg-gray-300 dark:bg-neutral-500 border-0" />
-        {posts.map((post) => (
-          <div key={post.id}>
-            <div className="flex justify-between text-start">
-              <Link href={`/blog/${post.slug}`}>
-                <p className="text-xs md:text-sm">
-                  ({new Date(post.published_at).toDateString()})
-                </p>
-                <h2 className="text-base md:text-2xl font-medium">
-                  {post.title}
-                </h2>
-                <p className="text-xs md:text-sm text-neutral-700 dark:text-secondary-white">
-                  {post.excerpt}
-                </p>
-              </Link>
-              <div className="text-xs md:text-sm">&#x1F866;</div>
-            </div>
-            <hr className="h-px my-8 bg-gray-300 dark:bg-neutral-500 border-0" />
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          <div className="lg:mr-[20%] mr-0 w-30">
+            <p className="uppercase text-[10px] text-stone-500">
+              (Musings, Shower-thoughts, Essays ... )
+            </p>
+            <p className="text-neutral-700 dark:text-secondary-white text-[13px] md:text-base ">
+              At certain times, articulating a concise portrayal of my identity
+              or occupation can prove to be challenging, as it encompasses a
+              complexity that cannot be adequately conveyed through a mere
+              handful of words.
+            </p>
           </div>
-        ))}
+          <div className="md:col-span-2">
+            <hr className="h-px lg:mb-8 my-8 lg:my-0  bg-gray-300 dark:bg-neutral-500 border-0" />
+            {posts.map((post) => (
+              <div key={post.id}>
+                <div className="flex justify-between text-start">
+                  <Link href={`/blog/${post.slug}`}>
+                    <p className="text-xs md:text-sm">
+                      ({new Date(post.published_at).toDateString()})
+                    </p>
+                    <h2 className="text-base md:text-2xl font-medium">
+                      {post.title}
+                    </h2>
+                    <p className="text-xs md:text-sm text-neutral-700 dark:text-secondary-white">
+                      {post.excerpt}
+                    </p>
+                  </Link>
+                  <div className="text-xs md:text-sm">&#x1F866;</div>
+                </div>
+                <hr className="h-px my-8 bg-gray-300 dark:bg-neutral-500 border-0" />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   )
