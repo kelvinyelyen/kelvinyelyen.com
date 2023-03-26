b0a5a798124d324cac9f0d2f758104dab4f959a0
Kelvin <kelvinyelyen@gmail.com>
2023-03-26 20:23:08 +0000

    Layout adjustments

--- utils/smoothScroll.js
+++ utils/smoothScroll.js
@@ -12,32 +12,29 @@ export default function Layout({ children, ...rest }) {
 
     const el = $content.current
 
-    if (window.innerWidth > 768) {
-      // only execute if viewport width is greater than 768px (adjust as needed)
-      scrollbar.current = SmoothScrollbar.init(el, {
-        damping: 0.04,
-        delegateTo: document,
-      })
+    scrollbar.current = SmoothScrollbar.init(el, {
+      damping: 0.04,
+      delegateTo: document,
+    })
 
-      scrollbar.current.setPosition(0, 0)
-      scrollbar.current.track.xAxis.element.remove()
+    scrollbar.current.setPosition(0, 0)
+    scrollbar.current.track.xAxis.element.remove()
 
-      ScrollTrigger.scrollerProxy(el, {
-        scrollTop(value) {
-          if (arguments.length) {
-            scrollbar.current.scrollTop = value
-          }
-          return scrollbar.current.scrollTop
-        },
-      })
-
-      scrollbar.current.addListener(ScrollTrigger.update)
-
-      return () => {
-        if (scrollbar.current) {
-          scrollbar.current.destroy()
-          scrollbar.current = null
+    ScrollTrigger.scrollerProxy(el, {
+      scrollTop(value) {
+        if (arguments.length) {
+          scrollbar.current.scrollTop = value
         }
+        return scrollbar.current.scrollTop
+      },
+    })
+
+    scrollbar.current.addListener(ScrollTrigger.update)
+
+    return () => {
+      if (scrollbar.current) {
+        scrollbar.current.destroy()
+        scrollbar.current = null
       }
     }
   }, [])
