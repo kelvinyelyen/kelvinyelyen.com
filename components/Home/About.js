38926d516008ae657c7f85c33c3c02aa8f449f50
Kelvin <kelvinyelyen@gmail.com>
2023-03-28 12:32:27 +0000

    Vercel deployment

--- components/Home/About.js
+++ components/Home/About.js
@@ -8,13 +8,13 @@ const About = () => {
       <div className={`${styles.aboutWidth} mx-auto mt-[80px] lg:mt-[80px]`}>
         <p className="text-[10px] text-stone-500 lg:mx-[90px]">(ABOUT)</p>
         <p className="uppercase dark:font-light text-[12px] lg:text-[15px] dark:text-secondary-white lg:mx-[90px]">
-          I&apos;m a software engineer and designer who&apos;s passion lies in
-          utilizing science and technology to create innovative solutions that
-          have a positive impact on the world. I believe that the human
-          experience, like the nautilus shell, is an ever-growing spiral that
-          allows us to build on our past experiences in order to meet and learn
-          from the demands of the present moment.
-          <Link href="/blog/2023-booklist"> Currently reading &#10042;</Link>
+          I&apos;m an indie software engineer and designer who&apos;s passion
+          lies in utilizing science and technology to create innovative
+          solutions that have a positive impact on the world. I believe that the
+          human experience, like the nautilus shell, is an ever-growing spiral
+          that allows us to build on our past experiences in order to meet and
+          learn from the demands of the present moment.
+          <Link href="/blog/2023-booklist"> &#10042;</Link>
         </p>
       </div>
     </section>
