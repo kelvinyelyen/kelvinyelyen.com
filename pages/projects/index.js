e5038a6d8d3a19471ff92321ac340aec6ab8f275
Kelvin <kelvinyelyen@gmail.com>
2023-03-29 16:00:34 +0000

    Updates

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,8 +1,10 @@
 import { AllProjects } from '@/components/Projects';
+import Meta from "@components/Meta"
 
 export default function Project() {
     return (
       <div>
+        <Meta title="Kelvin Yelyen"/>
         <AllProjects />
       </div>
     )
