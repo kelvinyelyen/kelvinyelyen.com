094087210c9cccb8ed2500a666797117886d84c7
kelvinyelyen <kelvinyelyen@gmail.com>
2023-04-15 22:45:31 +0000

    Added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,48 +1,15 @@
-import Layout from "@/components/Layout"
-import Script from "next/script"
-import { useRouter } from "next/router"
-import { useEffect } from "react"
-import * as gtag from "./gtag"
 import { ThemeProvider } from "next-themes"
-import Preloader from "@/components/Preloader"
+import Layout from "@/components/Layout"
 import "@/styles/globals.css"
+import Preloader from "@/components/Preloader"
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter()
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on("routeChangeComplete", handleRouteChange)
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange)
-    }
-  }, [router.events])
-
   return (
     <div>
       <div className="bg"></div>
       <Preloader />
       <ThemeProvider attribute="class">
         <Layout>
-          <Script
-            strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-          />
-          <Script
-            id="gtag-init"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
           <Component {...pageProps} />
         </Layout>
       </ThemeProvider>
