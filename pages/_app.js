da0653b73f425544b29c9ecc0dc0aaa5f6e81cf3
kelvinyelyen <kelvinyelyen@gmail.com>
2023-04-15 22:52:34 +0000

    Added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "next-themes"
 import Layout from "@/components/Layout"
+import Script from "next/script"
 import "@/styles/globals.css"
 import Preloader from "@/components/Preloader"
 
@@ -8,6 +9,24 @@ export default function App({ Component, pageProps }) {
     <div>
       <div className="bg"></div>
       <Preloader />
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-27MSMZKLNN"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-27MSMZKLNN', {
+            page_path: window.location.pathname,
+            });
+            `,
+        }}
+      />
       <ThemeProvider attribute="class">
         <Layout>
           <Component {...pageProps} />
