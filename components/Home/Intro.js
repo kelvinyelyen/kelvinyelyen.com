d8e61df9e8dc02098528fddf53cde7dc3921b049
kelvinyelyen <kelvinyelyen@gmail.com>
2023-04-17 18:37:28 +0000

    UI updates

--- components/Home/Intro.js
+++ components/Home/Intro.js
@@ -6,7 +6,7 @@ const Mobile = () => {
   return (
     <div className={`${styles.xPaddings} lg:-mt-12 mt-10 mx-auto`}>
       <div
-        className={`${styles.innerWidth} mx-auto my-[40px] lg:my-0 text-5xl lg:text-7xl`}
+        className={`${styles.innerWidth} mx-auto my-[40px] lg:my-0 text-4xl lg:text-7xl`}
       >
         <Link href="/">
           <h1 className="font-monument tracking-tighter dark:text-stone-300 text-neutral-800 mb-1">
