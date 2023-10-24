585e17e17f11ce1fb205c876f8cc79cbf511d6b7
kelvinyelyen <kelvinyelyen@gmail.com>
2023-10-24 18:22:59 +0000

    Minor updates

--- components/Projects/Project.js
+++ components/Projects/Project.js
@@ -1,29 +1,19 @@
 import Link from "next/link"
-import { useState } from "react"
 
 const Project = ({ title, url, description }) => {
-  const [isHovered, setIsHovered] = useState(false)
-
   return (
     <>
-      <Link href={url} target="_blank" className="my-4">
-        <div
-          className="grid md:grid-cols-2 grid-cols-1 text-[14px] md:text-[16px] border-b border-gray-200 dark:border-neutral-800 py-2 lg:py-3 relative"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
+      <Link
+        href={url}
+        target="_blank"
+        className="transition duration-200 ease-in-out hover:text-stone-500 my-4"
+      >
+        <div className="grid md:grid-cols-2 grid-cols-1 text-[14px] md:text-[16px] border-b border-gray-200 dark:border-neutral-800 py-2 lg:py-3 relative">
           <div className="font-medium">
             <h2>{title}</h2>
           </div>
           <div className="text-stone-500 font-thin md:text-end">
             {description}
-            <div
-              className={`absolute top-full left-0 bg-white text-black p-2 rounded shadow-md transition-opacity ${
-                isHovered ? "opacity-100" : "opacity-0"
-              }`}
-            >
-              {description}
-            </div>
           </div>
         </div>
       </Link>
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -4,8 +4,8 @@ import styles from "@/styles"
 
 export default function BlogPost({ posts }) {
   return (
-    <section className={`${styles.paddings} relative z-10 -mt-10`}>
-      <div className={`${styles.innerWidth} mx-auto dark:text-stone-200`}>
+    <section className={`${styles.paddings} relative z-10 -mt-10`}> 
+      <div className={`${styles.innerWidth} mx-auto dark:text-stone-200 mb-40`}>
         <div className="mb-6">
           <p className="text-[14px] md:text-[16px] ">
             Describing my identity or occupation concisely is challenging due to
@@ -17,13 +17,13 @@ export default function BlogPost({ posts }) {
           {posts.map((post) => (
             <div key={post.id}>
               <Link href={`/blog/${post.slug}`}>
-                <div className="flex justify-between gap-10 text-start border-b border-gray-200 dark:border-neutral-800 py-2 lg:py-3 ">
-                  <div>
+                <div className="grid grid-cols-9 border-b border-gray-200 dark:border-neutral-800 py-2 lg:py-3 ">
+                  <div className="col-span-8">
                     <h2 className="text-[14px] md:text-[16px] font-medium transition duration-200 ease-in-out hover:text-stone-500">
                       {post.title}
                     </h2>
                   </div>
-                  <div className="text-[10px] md:text-[16px] ">
+                  <div className="col-span-1 text-[10px] md:text-[16px] text-end">
                     <p className="text-stone-500">
                       {new Date(post.published_at).toLocaleDateString("en-US")}
                     </p>
