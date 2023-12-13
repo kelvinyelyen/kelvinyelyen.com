6ec05cbfd16a44d40df0d8d913bbc0b67d0ab6f7
kelvinyelyen <kelvinyelyen@gmail.com>
2023-12-13 19:01:46 +0000

    migration: app-router, ghostcms to mdx

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,3 +1,6 @@
+import fs from "fs"
+import path from "path"
+
 //TODO: Apply DRY principles
 export async function generateStaticParams() {
   await generateRssFeed()
@@ -29,10 +32,10 @@ function getPost({ slug }) {
 const URL = "https://kelvinyelyen.com"
 
 export default async function sitemap() {
-    let blogs = getPost().map((post) => ({
-      url: `{URL}/blog/${post.slug}`,
-      lastModified: post.metadata.publishedAt,
-    }))
+  let blogs = getPost().map((post) => ({
+    url: `{URL}/blog/${post.slug}`,
+    lastModified: post.metadata.publishedAt,
+  }))
 
   let routes = ["", "/projects", "/blog"].map((route) => ({
     url: `${URL}${route}`,
