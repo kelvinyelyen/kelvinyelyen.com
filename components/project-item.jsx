cd5147527eabeb3658c8a7af5ce046370e5ca92d
kelvinyelyen <kelvinyelyen@gmail.com>
2023-12-13 19:36:31 +0000

    migration: app-router, ghostcms to mdx

--- components/project-item.jsx
+++ components/project-item.jsx
@@ -10,7 +10,7 @@ export default function ProjectItem({ title, url, description }) {
       >
         <div className="grid md:grid-cols-3 grid-cols-1 text-sm border-b border-secondary-foreground py-2 lg:py-2 relative">
           <div className="col-span-1">
-            <h2>{title}</h2>
+            <h3>{title}</h3>
           </div>
           <div className="col-span-2 text-primary-foreground md:text-end text-xs ">
             {description}
