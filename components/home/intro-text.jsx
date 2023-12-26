0315456a0df217493307f07e61acd4f7efbfd438
kelvinyelyen <kelvinyelyen@gmail.com>
2023-12-26 21:27:19 +0000

    feature fix

--- components/home/intro-text.jsx
+++ components/home/intro-text.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function IntroText() {
   return (
     <div className="mx-auto">
-      <div className="prose prose-sm prose-invert max-w-none lg:leading-6 leading-relaxed">
+      <div className="prose prose-sm prose-stone prose-invert max-w-none lg:leading-6 leading-relaxed">
         <p>
           I&apos;m a software engineer and designer whose passion lies in
           utilizing science and technology to create innovative solutions that
