2c8c03c3512458c7793d4e8925d1ca759f8c36e1
kelvinyelyen <kelvinyelyen@gmail.com>
2024-04-10 12:08:30 +0000

    svg updates

--- app/page.js
+++ app/page.js
@@ -50,32 +50,105 @@ export default function Home() {
                   kelvinyelyen@gmail.com
                 </Link>
               </li>
-              <li className="transition duration-200 ease-in-out hover:text-stone-400">
-                <Link href="https://github.com/kelvinyelyen" target="_blank">
-                  GitHub &#129125;
+              <li className="flex justify-end transition duration-200 ease-in-out hover:text-stone-400">
+                <Link
+                  href="https://github.com/kelvinyelyen"
+                  target="_blank"
+                  className="flex align-center items-center"
+                >
+                  GitHub
+                  <span>
+                    <svg
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z"
+                        fill="currentColor"
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </span>
                 </Link>
               </li>
-              <li className="transition duration-200 ease-in-out hover:text-stone-400">
-                <Link href="https://twitter.com/kelvinyelyen" target="_blank">
-                  X (Twitter) &#129125;
+
+              <li className="flex justify-end transition duration-200 ease-in-out hover:text-stone-400">
+                <Link
+                  href="https://twitter.com/kelvinyelyen"
+                  target="_blank"
+                  className="flex align-center items-center"
+                >
+                  X (Twitter)
+                  <span>
+                    <svg
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z"
+                        fill="currentColor"
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </span>
                 </Link>
               </li>
-              <li className="transition duration-200 ease-in-out hover:text-stone-400">
+              <li className="flex justify-end transition duration-200 ease-in-out hover:text-stone-400">
                 <Link
                   href="https://www.linkedin.com/in/kelvinyelyen/"
                   target="_blank"
-                  className="transition duration-200 ease-in-out hover:text-stone-400"
+                  className="flex align-center items-center"
                 >
-                  Linkedin &#129125;
+                  Linkedin
+                  <span>
+                    <svg
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z"
+                        fill="currentColor"
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </span>
                 </Link>
               </li>
-              <li className="transition duration-200 ease-in-out hover:text-stone-400">
+              <li className="flex justify-end transition duration-200 ease-in-out hover:text-stone-400">
                 <Link
                   href="https://www.behance.net/kelvinyelyen"
                   target="_blank"
-                  className="transition duration-200 ease-in-out hover:text-stone-400"
+                  className="flex align-center items-center"
                 >
-                  Behance &#129125;
+                  Behance
+                  <span>
+                    <svg
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z"
+                        fill="currentColor"
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </span>
                 </Link>
               </li>
             </ul>
