6288d6135af79363a88dd448407da4ddd109b2be
Kelvin Yelyen <52675327+kelvinyelyen@users.noreply.github.com>
2024-05-11 09:26:20 +0000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -34,11 +34,11 @@ export default function Home() {
       <section className="mx-auto flex flex-col sm:flex-row justify-between my-14 lg:leading-6 leading-relaxed">
         <div className="uppercase mb-4">
           <p>
-            Design
+            Engineering
             <sup className="text-primary-foreground"> 01 </sup>
           </p>
           <p>
-            Engineering
+            Design
             <sup className="text-primary-foreground"> 02 </sup>
           </p>
         </div>
