38977875ee2853a957220217bc221e27e7eeca0e
kelvinyelyen <kelvinyelyen@gmail.com>
2024-06-21 20:21:33 +0000

    update: mafs library

---
+++ components/vector.jsx
@@ -0,0 +1,27 @@
+"use client"
+import "mafs/core.css"
+import "mafs/font.css"
+
+import { Mafs, Coordinates, useMovablePoint, Vector, vec } from "mafs"
+
+function VectorExample() {
+  const tip = useMovablePoint([0.4, 0.6])
+
+  const vec1 = tip.point
+  const angle = Math.atan2(tip.y, tip.x)
+  const vec2 = vec.add(vec1, vec.rotate(vec1, angle))
+  const vec3 = vec.add(vec1, vec.rotate(vec2, -2 * angle))
+
+  return (
+    <Mafs>
+      <Coordinates.Cartesian />
+      <Vector tip={vec1} />
+      <Vector tail={vec1} tip={vec2} />
+      <Vector tail={vec2} tip={vec3} />
+
+      {tip.element}
+    </Mafs>
+  )
+}
+
+export default VectorExample
--- lib/mdx.jsx
+++ lib/mdx.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable react/display-name */
+import dynamic from "next/dynamic"
 import React from "react"
 import Link from "next/link"
 import Image from "next/image"
 import { MDXRemote } from "next-mdx-remote/rsc"
 import { TweetComponent } from "@/components/tweet/tweet"
 import { highlight } from "sugar-high"
+
+const VectorExample = dynamic(() => import("@/components/vector"), {
+  ssr: false,
+})
 
 function Table({ data }) {
   let headers = data.headers.map((header, index) => (
@@ -170,6 +175,7 @@ let components = {
   StaticTweet: TweetComponent,
   code: Code,
   Table,
+  VectorExample,
 }
 
 export function CustomMDX(props) {
