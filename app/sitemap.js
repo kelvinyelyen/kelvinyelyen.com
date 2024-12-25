f7309b7c597d1001fa8f6fc0f293ff52c65d44c3
kelvinyelyen <kelvinyelyen@gmail.com>
2024-12-25 18:38:22 +0000

    feat: mdx content

--- app/sitemap.js
+++ app/sitemap.js
@@ -2,7 +2,8 @@ import { getContent } from "@/lib/content-handler"
 
 const URL = "https://kelvinyelyen.com"
 
-export default async function sitemap() {
+export default async function sitemap(params) {
+  const { slug } = await params
   let blogs = getContent({ category: "journal", slug }).map((post) => ({
     url: `https://kelvinyelyen.com/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
