57306377b2c580ce46f058a984e863b52a89c781
kelvinyelyen <kelvinyelyen@gmail.com>
2024-12-25 19:14:24 +0000

    feat: mdx content

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,21 +1,25 @@
-import { getContent } from "@/lib/content-handler"
+import { getCategoryContent } from "@/lib/content-handler"
 
 const URL = "https://kelvinyelyen.com"
 
-export default async function sitemap(params) {
-  const { slug } = await params
-
-  const blogs = await getContent({ category: "journal", slug })
+export default function sitemap() {
+  const blogs =  getCategoryContent("journal")
+  const reading = getCategoryContent("reading")
 
   const blogPosts = blogs.map((post) => ({
     url: `${URL}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }))
 
+    const readingList = reading.map((list) => ({
+      url: `${URL}/reading/${list.slug}`,
+      lastModified: list.metadata.publishedAt,
+    }))
+
   const routes = ["", "/work", "/blog", "/reading"].map((route) => ({
     url: `${URL}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }))
 
-  return [...routes, ...blogPosts]
+  return [...routes, ...blogPosts, ...readingList]
 }
