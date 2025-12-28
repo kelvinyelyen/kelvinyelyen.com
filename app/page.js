d4be8a1586f799d0cf887159ca96062de9a85933
Kelvin Yelyen <52675327+kelvinyelyen@users.noreply.github.com>
2025-12-28 16:56:46 +0000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="mx-auto">
           <div className="prose-sm max-w-none lg:leading-6 leading-relaxed">
             <p>
-               I build computational models at the intersection of artificial intelligence and neuroscience. My work focuses on reverse-engineering biological intelligence to understand how it emerges in neural systems and to design more adaptive machine learning architectures—drawing on neural decoding, brain-inspired design, and computational models of intelligence.{" "}
+              I build computational models at the intersection of artificial intelligence and neuroscience. My work focuses on reverse-engineering biological intelligence to understand how it emerges in neural systems and to design more adaptive machine learning architectures—drawing on neural decoding, brain-inspired design, and computational models of intelligence.{" "}
             </p>
           </div>
         </div>
@@ -16,7 +16,7 @@ export default function Home() {
 
       <section className="mx-auto my-10 lg:leading-6 leading-relaxed">
         <ul className="text-muted-foreground flex flex-row space-x-4">
-          <LinkItem href="https://drive.google.com/file/d/1PfEDoALLGI4S99Meb0zAqh_GMxPMFB7s/view?usp=sharing">CV</LinkItem>
+          <LinkItem href="https://drive.google.com/file/d/1PfEDoALLGI4S99Meb0zAqh_GMxPMFB7s/view?usp=sharing">Read CV</LinkItem>
           <LinkItem href="https://github.com/kelvinyelyen">GitHub</LinkItem>
           <LinkItem href="https://x.com/kelvinyelyen">X</LinkItem>
           <LinkItem href="https://www.linkedin.com/in/kelvinyelyen/">
